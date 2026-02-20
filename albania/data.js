86a8101376c3f25c137284da7dfd89ad79c02ba7
88romaitaly <88romaitaly@gmail.com>
2026-02-20 21:10:24 +0700

    Update data.js

--- albania/data.js
+++ albania/data.js
@@ -7,6 +7,12 @@ const albaniaLottoData = {
     // Array of results - MAX 30 ENTRIES
     results: [
         {
+            date: "2026-02-20",
+            draw: 293,
+            numbers: [7, 6, 1, 3],
+            time: "14:00"
+        },
+        {
             date: "2026-02-19",
             draw: 292,
             numbers: [8, 7, 6, 1],
@@ -178,12 +184,6 @@ const albaniaLottoData = {
             date: "2026-01-22",
             draw: 265,
             numbers: [5, 5, 2, 7],
-            time: "14:00"
-        }, 
-        {
-            date: "2026-01-21",
-            draw: 264,
-            numbers: [9, 3, 2, 4],
             time: "14:00"
         }
         // IMPORTANT: Keep only 30 entries maximum
