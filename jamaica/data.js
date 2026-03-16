8bd1a33d13647efe048d9380699ad143c488b912
88romaitaly <88romaitaly@gmail.com>
2026-03-17 04:59:49 +0700

    Update data.js

--- jamaica/data.js
+++ jamaica/data.js
@@ -11,6 +11,12 @@ const jamaicaLottoData = {
         
         // January 2026 - Example data
         {
+            date: "2026-03-17",
+            draw: 449,
+            numbers: [8, 6, 8, 1],
+            time: "17:00"
+        },
+        {
             date: "2026-03-16",
             draw: 448,
             numbers: [5, 9, 6, 7],
@@ -224,12 +230,6 @@ const jamaicaLottoData = {
             date: "2026-02-06",
             draw: 410,
             numbers: [3, 0, 5, 1],
-            time: "17:00"
-        },
-        {
-            date: "2026-02-05",
-            draw: 409,
-            numbers: [4, 9, 4, 0],
             time: "17:00"
         }
         // December 2025 - Example continuation
