61c1246c51cd188ec0d1343410320eb31bd4277a
88romaitaly <88romaitaly@gmail.com>
2026-04-16 15:02:00 +0700

    Update data.js

--- asmat/data.js
+++ asmat/data.js
@@ -12,6 +12,12 @@ const asmatLottoData = {
         // January 2026 - Example data
 
         {
+            date: "2026-04-16",
+            draw: 290,
+            numbers: [7, 8, 0, 0],
+            time: "15:00"
+        },
+        {
             date: "2026-04-15",
             draw: 289,
             numbers: [1, 6, 2, 1],
