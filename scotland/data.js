5a30ebb6c22055b22f6c3eec524a1cdec875e484
88romaitaly <88romaitaly@gmail.com>
2026-05-27 18:43:56 +0700

    Update data.js

--- scotland/data.js
+++ scotland/data.js
@@ -8,6 +8,12 @@ const scotlandLottoData = {
 
         
         {
+            date: "2026-05-27,
+            draw: 375,
+            numbers: [3, 9, 1, 9],
+            time: "11:30"
+        },
+        {
             date: "2026-05-26,
             draw: 374,
             numbers: [5, 1, 7, 7],
