3452654b4a802d8ef4e976c8a388937ac3717418
88romaitaly <88romaitaly@gmail.com>
2026-06-26 05:07:22 +0700

    Update data.js

--- jamaica/data.js
+++ jamaica/data.js
@@ -11,6 +11,12 @@ const jamaicaLottoData = {
         
         // Jun 2026 - Example data
         {
+            date: "2026-06-26",
+            draw: 548,
+            numbers: [5, 0, 4, 4],
+            time: "17:00"
+        },
+        {
             date: "2026-06-25",
             draw: 547,
             numbers: [2, 8, 3, 6],
