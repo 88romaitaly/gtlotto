b56bfb7813d598d970f6b986e7d950f8b7d28bff
88romaitaly <88romaitaly@gmail.com>
2026-07-28 05:06:10 +0700

    Update data.js

--- solomon/data.js
+++ solomon/data.js
@@ -7,6 +7,10 @@ const lotteryData = {
     results: [
         // 2026
         {
+            date: "2026-07-28",
+            numbers: [0, 9, 4, 4]
+        },
+        {
             date: "2026-07-27",
             numbers: [0, 7, 3, 6]
         },
