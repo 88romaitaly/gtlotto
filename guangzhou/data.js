1cb842d8a364db8111656acde64b381a6a50ab69
88romaitaly <88romaitaly@gmail.com>
2026-09-08 04:43:56 +0700

    Update data.js

--- guangzhou/data.js
+++ guangzhou/data.js
@@ -5,6 +5,7 @@ const guangzhouLottoData = {
         
 
         // Data untuk SEPTEMBER 2026
+        { date: "2026-09-07", numbers: [1, 7, 2, 3] },
         { date: "2026-09-06", numbers: [9, 5, 8, 5] },
         { date: "2026-09-05", numbers: [4, 3, 0, 7] },
         { date: "2026-09-04", numbers: [5, 7, 4, 2] },
